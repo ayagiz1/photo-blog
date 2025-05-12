0509e368b52dac0c1845afde6b76a119cfc87c9d
ayagiz1 <ahmetyagiizaydin@gmail.com>
2025-05-12 17:12:51 +0300

    routes created

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const express =  require("express");
 
 const app = express(); // req res kontrol ettigimiz uygulama olusturduk
 
+const path = require("path");
+
+app.use("/articles", function(req, res) {
+    res.sendFile(path.join(__dirname, "Pages/HTML", "articles.html"))
+});
+
+app.use("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "Pages/HTML", "contact.html"))
+});
+
+app.use("/", function(req, res) {
+    console.log("__dirname");
+    console.log("__filename");
+    res.sendFile(path.join(__dirname, "Pages/HTML", "index.html"))
+});
+
 app.listen(3000, function(){  // aktif porttaki requestleri alabilmek icin 3000 portunu dinliyoruz
     console.log("Listening on port 3000");  //Gelen reqlere sonra res gonderecegiz.
 })
 
-app.use(function(req, res){  // herhangi bir req istegi icin res gonderiyoruz
-    res.end(`Hello World`);
-});
